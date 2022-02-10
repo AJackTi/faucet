05e394185deb4e3301ea1adb46a0e806b2e1bd05
AJackTi <dtrong97vn@gmail.com>
2022-02-10 15:14:06 +0700

    Funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,9 +2,20 @@
 pragma solidity ^0.8.0;
 
 contract Faucet {
+    address[] public funders;
+
     receive() external payable {}
 
     function addFunds() external payable {
+        funders.push(msg.sender);
+    }
 
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
     }
 }
