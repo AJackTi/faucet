176ec2a42a7daef8b82611283ddd24d7c5bab2b3
AJackTi <dtrong97vn@gmail.com>
2022-02-17 23:48:06 +0700

    lutFunders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,26 +3,32 @@ pragma solidity ^0.8.0;
 
 contract Faucet {
     uint public numberOfFunders;
-    mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numberOfFunders++;
-        funders[index] = msg.sender;
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            uint index = numberOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
     }
 
     function getAllFunders() external view returns (address[] memory) {
         address[] memory _funders = new address[](numberOfFunders);
 
         for (uint i = 0; i < numberOfFunders; i++) {
-            _funders[i] = funders[i];
+            _funders[i] = lutFunders[i];
         }
 
         return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns (address) {
-        return funders[index];
+        return lutFunders[index];
     }
 }
