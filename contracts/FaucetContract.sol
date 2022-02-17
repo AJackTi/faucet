082a8b2b08724cf349377cc90270f1419643e4c7
AJackTi <dtrong97vn@gmail.com>
2022-02-17 23:25:54 +0700

    Get all funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,20 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract Faucet {
-    address[] public funders;
+    uint public numberOfFunders;
+    mapping(uint => address) private funders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        funders.push(msg.sender);
+        uint index = numberOfFunders++;
+        funders[index] = msg.sender;
     }
 
-    function getAllFunders() public view returns (address[] memory) {
-        return funders;
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numberOfFunders);
+
+        for (uint i = 0; i < numberOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+
+        return _funders;
     }
 
-    function getFunderAtIndex(uint8 index) external view returns(address) {
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        return funders[index];
     }
 }
